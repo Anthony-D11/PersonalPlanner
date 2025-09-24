fc59c32fa7cd6995c99dd7e788343f2c4d58ab6b
Anthony-D11 <hungdao1152002@gmail.com>
2025-09-24 12:04:26 -0300

    Modify prod api server url and deploy mode

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ec2-3-17-39-124.us-east-2.compute.amazonaws.com:8080/api/v1'
+    apiUrl: 'http://ec2-3-17-39-124.us-east-2.compute.amazonaws.com:8080/api/v1'
 };
