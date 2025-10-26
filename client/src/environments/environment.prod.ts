27e3d890139df9cc75d0831181b62eb38637fe99
Anthony-D11 <hungdao1152002@gmail.com>
2025-10-26 13:43:31 -0300

    Update api endpoint

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://personal-planner-server.onrender.com'
+    apiUrl: 'https://personal-planner-server.onrender.com/api/v1'
 };
